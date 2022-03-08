d24dae37183d830ebaee381fed9c5499e6797357
zhangyuxuan28 <zhangyuxuan28@jd.com>
2022-03-08 12:42:35 +0800

    初始化

---
+++ src/test/java/com/zhangyuxuan/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangyuxuan.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
